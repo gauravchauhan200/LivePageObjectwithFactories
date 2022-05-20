68f5b77919276d9bf557ca62f383e24100b4bd12
Gaurav chauhan <gauravchauhan200@gmail.com>
2022-05-20 15:02:36 +0530

    selenium 3.141 works only with WebDriverManager5.1.1

--- src/main/java/com/w2a/pages/HomePage.java
+++ src/main/java/com/w2a/pages/HomePage.java
@@ -1,31 +1,48 @@
 package com.w2a.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class HomePage 
+public class HomePage
 {
 	WebDriver driver;
-	
+
 	public HomePage(WebDriver driver)
 	{
 		this.driver = driver;
 	}
+
 	
-	public void findNewCar()
+	public void findNewCar() throws Exception 
 	{
-		WebElement newCars= driver.findElement(By.xpath("//*[@id='root']/div[1]/header/div/nav/ul/li[1]/div"));
-		new Actions(driver).moveToElement(newCars).perform();
-		driver.findElement(By.xpath("//*[@id='root']/div[1]/header/div/nav/ul/li[1]/div[2]/div[1]/ul/a[1]/div/div")).click();
+		WebElement newCars= driver.findElement(By.xpath("//div[text()='NEW CARS']"));
+		Actions action=new Actions(driver);
+		action.moveToElement(newCars).perform();
+	
+		List<WebElement> newCarOptions = driver.findElements(By.xpath("//ul[@class='o-cpnuEd o-XylGE JP2Cxm']/a/div/div"));
+			for(WebElement option:newCarOptions)
+			{
+				if(option.getText().equalsIgnoreCase("New Launches"))
+				{
+					option.click();
+					break;
+				}
+			}
+			Thread.sleep(3000);
 	}
+
+	
 	
 	public void searchNewCar(String carname)
 	{
-		driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div[1]/div/input")).sendKeys(carname);
-		driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/div[2]/div/div[1]/div/button")).click();
+		driver.findElement(By.xpath("//input[@type='text' and @class='o-dqBOwT o-cKuOoN o-eZCpVk o-bfyaNx o-eZTujG o-jjpuv o-fzptVd o-fzptYr o-fEEqXL o-eKWNKE o-bIMsfE o-ItVGT  o-fznJFI DcJpYI bAREi7 mX5y3k  DcJpYI  ']")).sendKeys(carname);
+		driver.findElement(By.xpath("//*[@id='root']/div[3]/div[1]/div[2]/div/div[1]/div/button")).click();
 	}
+
 	
 	public void searchUsedCar(String city)
 	{
@@ -33,8 +50,7 @@ public class HomePage
 		driver.findElement(By.xpath("//span[@data-unique-key='used']")).click();
 		driver.findElement(By.xpath("//input[@placeholder='City, eg: Mumbai']")).sendKeys(city);
 		driver.findElement(By.xpath("//button[@type='button']")).click();
-		
 	}
-	
-	
+
+
 }
--- src/test/java/com/w2a/testcases/FindNewCarTest.java
+++ src/test/java/com/w2a/testcases/FindNewCarTest.java
@@ -1,36 +1,45 @@
 package com.w2a.testcases;
 
-import java.time.Duration;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.w2a.pages.HomePage;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class FindNewCarTest 
+
+public class FindNewCarTest
 {
 
-	public static void main (String[] args)
+	public static void main (String[] args) throws Exception
 	{
 
 		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
-		driver = new ChromeDriver();
+	//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("profile.default_content_setting_values.notifications", 2);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		driver= new ChromeDriver(options);
 		
 		driver.get("https://www.carwale.com/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+		
 		HomePage home = new HomePage(driver);
 		home.findNewCar();
-	
-	
-		
+
 		
 		driver.quit();
 	}
-	
+
 }
