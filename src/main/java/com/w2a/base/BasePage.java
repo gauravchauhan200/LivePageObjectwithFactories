0952f1b5917746cfeb56ed7e4b3a85fe5a4eb744
Gaurav chauhan <gauravchauhan200@gmail.com>
2022-05-23 23:20:55 +0530

    BaseTestSetup completly

--- src/main/java/com/w2a/base/BasePage.java
+++ src/main/java/com/w2a/base/BasePage.java
@@ -8,26 +8,20 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage 
 {
-	public WebDriver driver;
+	public static WebDriver driver;  
 	
 	public BasePage(WebDriver driver)
 	{
 		this.driver=driver;	
 		PageFactory.initElements(driver,this);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 	
 }
+
+
+
+/*	we made WebDriver static because we need it in
+ *  ExtentManagerclass and if we make BaseTest driver static,
+ * 	our testcase might not run in parallel
+ */
--- src/test/java/com/w2a/testcases/FindNewCarTest.java
+++ src/test/java/com/w2a/testcases/FindNewCarTest.java
@@ -1,16 +1,17 @@
 package com.w2a.testcases;
-
 import org.testng.annotations.Test;
 
 import com.w2a.base.BaseTest;
 import com.w2a.pages.HomePage;
 import com.w2a.pages.NewCarPage;
 
+
 public class FindNewCarTest extends BaseTest
 {
 	@Test
 	public void findNewCar() throws InterruptedException
 	{	
+		setUp("chrome");
 		HomePage home = new HomePage(driver);
 		NewCarPage carPage = home.findNewCar();
 		carPage.gotoBMW();
